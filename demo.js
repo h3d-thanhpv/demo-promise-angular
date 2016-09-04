ea79c92fb7f4325bf086dde243d7649f89534ec8
Phan Van Thanh <thanhpv@fimo.edu.vn>
2016-09-05 06:34:49 +0700

    Watch variable "listBooks" for update book info :boom:

--- demo.js
+++ demo.js
@@ -8,10 +8,14 @@ projectApp.controller('loadBookCtrl', ['$scope', '$http', '$filter', function($s
             url: url
         }).success(function (data) {
             $scope.listBooks = data.items;
-            $scope.book = $scope.listBooks[0];
         }).error(function (data, status, headers, config) {
         });
     }
     $scope.baseUrl = "http://localhost/yii2-training/api/v1/books";
     getBookInfo($scope.baseUrl);
+    $scope.$watch('listBooks', function (listBooks) {
+        if(angular.isDefined("listBooks") && listBooks.length > 0) {
+            $scope.book = $scope.listBooks[0];
+        }
+    });
 }]);
