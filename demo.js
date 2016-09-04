bef78d6b235c20b8ddbb865422d12fd05f263fe5
Phan Van Thanh <thanhpv@fimo.edu.vn>
2016-09-05 06:33:14 +0700

    Get book info after get list book success :dizzy:

--- demo.js
+++ demo.js
@@ -1,6 +1,17 @@
 var projectApp = angular.module('pkTrainApp', []);
 
 projectApp.controller('loadBookCtrl', ['$scope', '$http', '$filter', function($scope, $http, $filter) {
-    $scope.bookInfo = {};
+    $scope.listBooks = {};
+    function getBookInfo(url) {
+        $http({
+            method: 'GET',
+            url: url
+        }).success(function (data) {
+            $scope.listBooks = data.items;
+            $scope.book = $scope.listBooks[0];
+        }).error(function (data, status, headers, config) {
+        });
+    }
     $scope.baseUrl = "http://localhost/yii2-training/api/v1/books";
+    getBookInfo($scope.baseUrl);
 }]);
