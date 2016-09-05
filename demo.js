7452ae4a18d057a464157c4a4eed22473eaefe00
Phan Van Thanh <thanhpv@fimo.edu.vn>
2016-09-05 07:15:46 +0700

    use defer :star2:

--- demo.js
+++ demo.js
@@ -1,21 +1,20 @@
 var projectApp = angular.module('pkTrainApp', []);
 
-projectApp.controller('loadBookCtrl', ['$scope', '$http', '$filter', function($scope, $http, $filter) {
-    $scope.listBooks = {};
-    function getBookInfo(url) {
-        $http({
-            method: 'GET',
-            url: url
-        }).success(function (data) {
-            $scope.listBooks = data.items;
-        }).error(function (data, status, headers, config) {
-        });
-    }
-    $scope.baseUrl = "http://localhost/yii2-training/api/v1/books";
-    getBookInfo($scope.baseUrl);
-    $scope.$watch('listBooks', function (listBooks) {
-        if(angular.isDefined("listBooks") && listBooks.length > 0) {
-            $scope.book = $scope.listBooks[0];
-        }
+projectApp.controller('loadBookCtrl', ['$scope', '$http', '$filter', '$q', function($scope, $http, $filter, $q) {
+    var promise = getBookInfo("http://localhost/yii2-training/api/v1/books");
+    promise.then(function (data) {
+        $scope.book = data.items[0];
+    }, function (error) {
+        console.log(error);
     });
+
+    function getBookInfo(url) {
+        var defer = $q.defer();
+        $http.get(url).success(function(result) {
+            defer.resolve(result);
+        }).error(function (error) {
+            defer.reject(error);
+        });
+        return defer.promise;
+    }
 }]);
