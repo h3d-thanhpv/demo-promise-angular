f9c0077bdef9664272167d587bd5c5af22c930a7
Phan Van Thanh <thanhpv@fimo.edu.vn>
2016-09-05 07:41:45 +0700

    Best practice for $http :cupid:

--- demo.js
+++ demo.js
@@ -1,20 +1,9 @@
 var projectApp = angular.module('pkTrainApp', []);
 
 projectApp.controller('loadBookCtrl', ['$scope', '$http', '$filter', '$q', function($scope, $http, $filter, $q) {
-    var promise = getBookInfo("http://localhost/yii2-training/api/v1/books");
-    promise.then(function (data) {
-        $scope.book = data.items[0];
+    $http.get("http://localhost/yii2-training/api/v1/books").then(function (data) {
+        $scope.book = data.data.items[0];
     }, function (error) {
         console.log(error);
     });
-
-    function getBookInfo(url) {
-        var defer = $q.defer();
-        $http.get(url).success(function(result) {
-            defer.resolve(result);
-        }).error(function (error) {
-            defer.reject(error);
-        });
-        return defer.promise;
-    }
 }]);
